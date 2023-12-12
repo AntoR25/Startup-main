a5705119a559b1b4eac550a9151ae5b40a4c18a9
AntoR25 <antoine.remacle2407@gmail.com>
2023-12-12 02:33:38 +0100

    r2

--- pages/index.vue
+++ pages/index.vue
@@ -18,16 +18,16 @@ const { data: posts } = await useAsyncData('latest-posts', () =>
 </section>
 <section class="flex flex-col md:flex-row">
     <div class="md:w-3/4">
-        <h2 class="text-3xl font-bold mb-4 mt-2">Notre Histoire</h2>
-        <hr class="mt-3 border-t-2 border-blue-400 w-40">
+        <h2 class="text-3xl font-bold mb-4 mt-3">Notre Histoire</h2>
+        <hr class="mt-3 border-t-4 border-blue-400 w-40">
         <p class="text-lg py-1">
             Nous sommes une équipe passionnée chez Anime Character Sprite, spécialisée dans la création de sprites uniques tirés de vos animes préférés. Notre mission est de fournir des ressources graphiques de haute qualité aux créateurs, aux développeurs et aux amateurs d'anime, afin d'enrichir leurs projets visuels avec des personnages inoubliables.
         </p>
-        <hr class="mt-3 border-t-2 border-blue-400 w-40">
+        <hr class="mt-3 border-t-4 border-blue-400 w-40">
         <p class="text-lg py-1">
             Forts d'une expérience cumulée de plus de 20 ans, nous sommes des autodidactes convaincus que la passion et le travail acharné sont les clés du succès. Nous mettons nos connaissances et notre dévouement au service de la communauté des créateurs pour les aider à atteindre leurs objectifs.
         </p>
-        <hr class="mt-3 border-t-2 border-blue-400 w-40">
+        <hr class="mt-3 border-t-4 border-blue-400 w-40">
         <p class="text-lg py-1">
             Chez Anime Character Sprite, nous croyons en la diversité des personnages et nous nous engageons à offrir une bibliothèque variée et constamment mise à jour. Nous sommes fiers de contribuer à la réalisation de projets uniques et inspirants.
         </p>
@@ -37,7 +37,7 @@ const { data: posts } = await useAsyncData('latest-posts', () =>
 
   <section>
     <h2 class="text-3xl font-bold mt-8">Latest Blog Posts</h2>
-    <div class="grid md:grid-cols-3 pt-8 gap-10">
+    <div class="grid md:grid-cols-3 pt-8 gap-10 pb-8">
       <Post :posts="posts" />
     </div>
   </section>
